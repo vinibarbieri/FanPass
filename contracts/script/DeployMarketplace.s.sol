2bee3ecb4248125b5ba66d33599ace08738a0e45
vinibarbieri <vinicius190702@hotmail.com>
2025-04-14 22:15:01 -0300

    add: DeployMarketplace.s.sol

---
+++ contracts/script/DeployMarketplace.s.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import "../src/Marketplace.sol";
+
+contract DeployMarketplace is Script {
+    function run() external {
+        address ticketNFT = vm.envAddress("TICKET_NFT");
+        address platformReceiver = vm.envAddress("PLATFORM_RECEIVER");
+        address owner = vm.envAddress("INITIAL_OWNER");
+
+        vm.startBroadcast();
+
+        Marketplace marketplace = new Marketplace(ticketNFT, platformReceiver, owner);
+
+        vm.stopBroadcast();
+
+        console.log("Marketplace deployed at:", address(marketplace));
+    }
+}
